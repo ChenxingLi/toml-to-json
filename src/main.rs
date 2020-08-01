0de5696078d8d8fa6528779f69f82268d4841a52
ChenxingLi <lylcx2007@gmail.com>
2020-08-01 19:55:07 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,33 @@ use serde_json::to_string_pretty;
 use std::env;
 use std::error::Error;
 use std::fs::{read_to_string, write};
-use string_error::{into_err, new_err};
-use toml::Value as tomlVal;
+use string_error::into_err;
+use toml;
 
 pub fn covert_toml_to_json(toml_file: &str, json_file: &str) -> Result<(), String> {
     let toml = read_to_string(toml_file)
         .map_err(|e| format!("failed to read toml file: {:?}", e))?
-        .parse::<tomlVal>()
+        .parse::<toml::Value>()
         .map_err(|e| format!("failed to parse toml file: {:?}", e))?;
     let json = to_string_pretty(&toml)
         .map_err(|e| format!("failed to convert to json string: {:?}", e))?;
     write(&json_file, &json).map_err(|e| format!("failed to write to json file: {:?}", e))?;
+    println!("Done");
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!();
-        return Err(new_err("Incorrect argument numbers"));
-    }
+    let (toml_path, json_path) = match args.len() {
+        3 => (args[1].clone(), args[2].clone()),
+        2 => (args[1].clone(), args[1].clone() + ".json"),
+        _ => {
+            return Err(into_err(format!(
+                "Incorrect argument numbers {:?}",
+                args.len() - 1
+            )))
+        }
+    };
 
-    covert_toml_to_json(&args[0], &args[1]).map_err(|err| into_err(err))
+    covert_toml_to_json(&toml_path, &json_path).map_err(|err| into_err(err))
 }
